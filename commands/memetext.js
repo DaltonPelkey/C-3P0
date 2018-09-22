e46d58214d44080e7dfd9736ade4c759533d0cd4
= <=>
2018-09-22 17:33:07 -0400

    Add memetext command

---
+++ commands/memetext.js
@@ -0,0 +1,32 @@
+const { version } = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+
+exports.run = (client, message, args, level) => {
+    if (args.length < 1) return message.reply("You have to give me some text to meme-ify, silly :P");
+
+    message.delete()
+    .then(msg => msg.channel.send(memeText(args.join(" "))));
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "memetext",
+    category: "Miscelaneous",
+    description: "StUpIfY YoUr tExT",
+    usage: "memetext <message>"
+};
+
+function memeText (input) {
+    var res = "";
+    for (i=0; i < input.length; i++) {
+        res += i % 2 == 0 ? input.charAt(i).toUpperCase() : input.charAt(i);
+    }
+    return res;
+}
